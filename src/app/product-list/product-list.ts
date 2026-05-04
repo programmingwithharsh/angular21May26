8758e2c1c854ed55c2fea898e1ed61c70ac011e0
Harsh Tiwari <harshtiwari343@email.com>
2026-05-04 17:24:12 +0530

    Update pipe

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { Accordion } from "../accordion/accordion";
 import { Modal } from "../modal/modal";
 import { Star } from "../star/star";
+import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [Accordion, Modal, Star],
+  imports: [Accordion, Modal, Star, LowerCasePipe],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList {}
+export class ProductList {
+  productTitle: string = "Dashboard Title"; // property
+}
