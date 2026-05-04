b58c5ddd75a97529b98d0cef39c6f9eda7bc704b
Harsh Tiwari <harshtiwari343@email.com>
2026-05-04 16:57:15 +0530

    Added Bootstrap

---
+++ src/app/accordion/accordion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  imports: [],
+  templateUrl: './accordion.html',
+  styleUrl: './accordion.scss',
+})
+export class Accordion {}
--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Accordion } from "../accordion/accordion";
+import { Modal } from "../modal/modal";
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [Accordion, Modal],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
