9a100d6fb19f5108988db402a3b2af67bd70325d
Harsh Tiwari <harshtiwari343@email.com>
2026-05-04 17:02:03 +0530

    Added font awesome

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Accordion } from "../accordion/accordion";
 import { Modal } from "../modal/modal";
+import { Star } from "../star/star";
 
 @Component({
   selector: 'app-product-list',
-  imports: [Accordion, Modal],
+  imports: [Accordion, Modal, Star],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
