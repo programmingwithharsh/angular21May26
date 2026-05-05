4498f3786d13270dffba238f2684750207d58c2a
Harsh Tiwari <harshtiwari343@email.com>
2026-05-05 10:31:30 +0530

    Added interface

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -3,6 +3,7 @@ import { Accordion } from "../accordion/accordion";
 import { Modal } from "../modal/modal";
 import { Star } from "../star/star";
 import { LowerCasePipe } from '@angular/common';
+import { Iproduct } from '../iproduct';
 
 @Component({
   selector: 'app-product-list',
@@ -12,4 +13,60 @@ import { LowerCasePipe } from '@angular/common';
 })
 export class ProductList {
   productTitle: string = "Dashboard Title"; // property
+  x: number = 2;
+  y: any = "2";
+  numbers: number[] = [301, 302, 303, 304, 305];
+  flowers: string[] = ["Rose", "Dahlia", "Magnolia", "Tulip", "Daisy"];
+  products: Iproduct[] = [
+    {
+      "productId": 1,
+      "productName": "Leaf Rake - JSON",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    },
+    {
+      "productId": 3,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    },
+    {
+      "productId": 4,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+  ];
 }
